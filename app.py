0e7f02a391e9b742364b95482bc3ced21673a3e0
GAURAV GOSWAMI <64435441+piyush8884@users.noreply.github.com>
2026-03-26 20:50:03 +0530

    Initialize Flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+   return {
+       "message": "Hello from LOCAL SYSTEM",
+       "host": socket.gethostname()
+   }
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=5000)
